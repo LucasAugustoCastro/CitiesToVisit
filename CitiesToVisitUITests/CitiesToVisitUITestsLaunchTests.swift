586be509f7984cde4f035abfc2b12aa234f7a218
Lucas Castro <lucasaugustocastro24@gmail.com>
2023-06-24 19:22:09 -0300

    Initial Commit

---
+++ CitiesToVisitUITests/CitiesToVisitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CitiesToVisitUITestsLaunchTests.swift
+//  CitiesToVisitUITests
+//
+//  Created by Lucas Castro on 24/06/23.
+//
+
+import XCTest
+
+final class CitiesToVisitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
